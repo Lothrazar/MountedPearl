367aef71187196983412587816ed45d914931df6
sam <samson.bassett@gmail.com>
2019-08-04 09:36:52 -0700

    1.14.4 cancel damage and reformat

--- src/main/java/com/lothrazar/samsmountedpearl/ModMountedPearl.java
+++ src/main/java/com/lothrazar/samsmountedpearl/ModMountedPearl.java
@@ -1,5 +1,4 @@
 package com.lothrazar.samsmountedpearl;
-
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -7,70 +6,62 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.EnderTeleportEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod; 
+import net.minecraftforge.fml.common.Mod;
 
-@Mod( ModMountedPearl.MODID)
-public class ModMountedPearl{
+@Mod(ModMountedPearl.MODID)
+public class ModMountedPearl {
 
-	public static final String MODID = "mountedpearl";
+  public static final String MODID = "mountedpearl";
+  public static final String NBT_RIDING_ENTITY = "ride";
 
-	public static final String NBT_RIDING_ENTITY = "ride";
+  public ModMountedPearl() {
+    MinecraftForge.EVENT_BUS.register(this);
+  }
 
-	public ModMountedPearl(){
-		MinecraftForge.EVENT_BUS.register(this);
-	}
+  @SubscribeEvent
+  public void onEnderTeleportEvent(EnderTeleportEvent event) {
+    Entity ent = event.getEntity();
+    if (ent instanceof LivingEntity == false) {
+      return;
+    }
+    LivingEntity living = (LivingEntity) event.getEntity();
+    if (living == null) {
+      return;
+    }
+    if (living.world.isRemote == false)// do not spawn a second 'ghost' one on client side
+    {
+      if (living.getRidingEntity() != null && living instanceof PlayerEntity) {
+        PlayerEntity player = (PlayerEntity) living;
+        player.getEntityData().putInt(NBT_RIDING_ENTITY, player.getRidingEntity().getEntityId());
+        player.getRidingEntity().setPositionAndUpdate(event.getTargetX(), event.getTargetY(), event.getTargetZ());
+        event.setAttackDamage(0);
+      }
+    }
+  }
 
-	@SubscribeEvent
-	public void onEnderTeleportEvent(EnderTeleportEvent event){
-
-		Entity ent = event.getEntity();
-		if(ent instanceof LivingEntity == false){
-			return;
-		}
-		LivingEntity living = (LivingEntity) event.getEntity();
-		if(living == null){
-			return;
-		}
-
-		if(living.world.isRemote == false)// do not spawn a second 'ghost' one on client side
-		{
-			if(living.getRidingEntity() != null && living instanceof PlayerEntity){
-				PlayerEntity player = (PlayerEntity) living;
-
-				player.getEntityData().putInt(NBT_RIDING_ENTITY, player.getRidingEntity().getEntityId());
-
-				player.getRidingEntity().setPositionAndUpdate(event.getTargetX(), event.getTargetY(), event.getTargetZ());
-			}
-		}
-	}
-
-	@SubscribeEvent
-	public void onEntityUpdate(LivingUpdateEvent event){
-
-		Entity ent = event.getEntity();
-		if(ent instanceof LivingEntity == false){
-			return;
-		}
-		LivingEntity living = (LivingEntity) event.getEntity();
-		if(living == null){
-			return;
-		}
-
-		if(living instanceof PlayerEntity){
-			PlayerEntity player = (PlayerEntity) living;
-			if(player.getEntityData() == null){
-				return;
-			}
-			int setride = player.getEntityData().getInt(NBT_RIDING_ENTITY);
-
-			if(setride > 0 && player.getRidingEntity() == null){
-				Entity horse = player.world.getEntityByID(setride);
-
-				if(horse != null){
-					player.startRiding(horse, true);
-					player.getEntityData().putInt(NBT_RIDING_ENTITY, -1);
-				}
-			}
-		}
-	}
+  @SubscribeEvent
+  public void onEntityUpdate(LivingUpdateEvent event) {
+    Entity ent = event.getEntity();
+    if (ent instanceof LivingEntity == false) {
+      return;
+    }
+    LivingEntity living = (LivingEntity) event.getEntity();
+    if (living == null) {
+      return;
+    }
+    if (living instanceof PlayerEntity) {
+      PlayerEntity player = (PlayerEntity) living;
+      if (player.getEntityData() == null) {
+        return;
+      }
+      int setride = player.getEntityData().getInt(NBT_RIDING_ENTITY);
+      if (setride > 0 && player.getRidingEntity() == null) {
+        Entity horse = player.world.getEntityByID(setride);
+        if (horse != null) {
+          player.startRiding(horse, true);
+          player.getEntityData().putInt(NBT_RIDING_ENTITY, -1);
+        }
+      }
+    }
+  }
 }
