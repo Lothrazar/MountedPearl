7b1f4b61da7535b746247e219f813c57d651f161
sam <samson.bassett@gmail.com>
2019-08-04 09:25:50 -0700

    1.14.4 first update

--- src/main/java/com/lothrazar/samsmountedpearl/ModMountedPearl.java
+++ src/main/java/com/lothrazar/samsmountedpearl/ModMountedPearl.java
@@ -1,50 +1,43 @@
 package com.lothrazar.samsmountedpearl;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.EnderTeleportEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod; 
 
-@Mod(modid = ModMountedPearl.MODID, useMetadata = true, updateJSON = "https://raw.githubusercontent.com/LothrazarMinecraftMods/MountedPearl/master/update.json")
+@Mod( ModMountedPearl.MODID)
 public class ModMountedPearl{
 
-	public static final String MODID = "samsmountedpearl";
-	@Instance(value = MODID)
-	public static ModMountedPearl instance;
+	public static final String MODID = "mountedpearl";
+
 	public static final String NBT_RIDING_ENTITY = "ride";
 
-	@EventHandler
-	public void onPreInit(FMLPreInitializationEvent event){
-
-		MinecraftForge.EVENT_BUS.register(instance);
+	public ModMountedPearl(){
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	@SubscribeEvent
 	public void onEnderTeleportEvent(EnderTeleportEvent event){
 
 		Entity ent = event.getEntity();
-		if(ent instanceof EntityLiving == false){
+		if(ent instanceof LivingEntity == false){
 			return;
 		}
-		EntityLivingBase living = (EntityLivingBase) event.getEntity();
+		LivingEntity living = (LivingEntity) event.getEntity();
 		if(living == null){
 			return;
 		}
 
-		if(living.worldObj.isRemote == false)// do not spawn a second 'ghost' one on client side
+		if(living.world.isRemote == false)// do not spawn a second 'ghost' one on client side
 		{
-			if(living.getRidingEntity() != null && living instanceof EntityPlayer){
-				EntityPlayer player = (EntityPlayer) living;
+			if(living.getRidingEntity() != null && living instanceof PlayerEntity){
+				PlayerEntity player = (PlayerEntity) living;
 
-				player.getEntityData().setInteger(NBT_RIDING_ENTITY, player.getRidingEntity().getEntityId());
+				player.getEntityData().putInt(NBT_RIDING_ENTITY, player.getRidingEntity().getEntityId());
 
 				player.getRidingEntity().setPositionAndUpdate(event.getTargetX(), event.getTargetY(), event.getTargetZ());
 			}
@@ -55,27 +48,27 @@ public class ModMountedPearl{
 	public void onEntityUpdate(LivingUpdateEvent event){
 
 		Entity ent = event.getEntity();
-		if(ent instanceof EntityLiving == false){
+		if(ent instanceof LivingEntity == false){
 			return;
 		}
-		EntityLivingBase living = (EntityLivingBase) event.getEntity();
+		LivingEntity living = (LivingEntity) event.getEntity();
 		if(living == null){
 			return;
 		}
 
-		if(living instanceof EntityPlayer){
-			EntityPlayer player = (EntityPlayer) living;
+		if(living instanceof PlayerEntity){
+			PlayerEntity player = (PlayerEntity) living;
 			if(player.getEntityData() == null){
 				return;
 			}
-			int setride = player.getEntityData().getInteger(NBT_RIDING_ENTITY);
+			int setride = player.getEntityData().getInt(NBT_RIDING_ENTITY);
 
 			if(setride > 0 && player.getRidingEntity() == null){
-				Entity horse = player.worldObj.getEntityByID(setride);
+				Entity horse = player.world.getEntityByID(setride);
 
 				if(horse != null){
 					player.startRiding(horse, true);
-					player.getEntityData().setInteger(NBT_RIDING_ENTITY, -1);
+					player.getEntityData().putInt(NBT_RIDING_ENTITY, -1);
 				}
 			}
 		}
