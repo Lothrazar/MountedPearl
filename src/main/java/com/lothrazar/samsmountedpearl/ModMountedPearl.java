03696ca0e9298f3d9f8529bae663ab2446ac8fd2
lothrazar <samson.bassett@gmail.com>
2019-12-21 22:21:48 -0800

    1.15 ported

--- src/main/java/com/lothrazar/samsmountedpearl/ModMountedPearl.java
+++ src/main/java/com/lothrazar/samsmountedpearl/ModMountedPearl.java
@@ -14,6 +14,7 @@ public class ModMountedPearl {
 
   public static final String MODID = "mountedpearl";
   public static final String NBT_RIDING_ENTITY = "ride";
+  public static final String NBT_RIDING_TIMER = "ridetimer";
 
   public ModMountedPearl() {
     MinecraftForge.EVENT_BUS.register(this);
@@ -33,8 +34,12 @@ public class ModMountedPearl {
       if (living.getRidingEntity() != null && living instanceof PlayerEntity) {
         PlayerEntity player = (PlayerEntity) living;
         player.getPersistentData().putInt(NBT_RIDING_ENTITY, player.getRidingEntity().getEntityId());
-        System.out.println("setpos after teleport " + player.getRidingEntity());
-        player.getRidingEntity().setPositionAndUpdate(event.getTargetX(), event.getTargetY(), event.getTargetZ());
+        player.stopRiding();
+        player.getPersistentData().putInt(NBT_RIDING_TIMER, 3);
+        //                player.getRidingEntity().setPositionAndUpdate(event.getTargetX(), event.getTargetY(), event.getTargetZ());
+        player.getPersistentData().putDouble("mpx", event.getTargetX());
+        player.getPersistentData().putDouble("mpy", event.getTargetY());
+        player.getPersistentData().putDouble("mpz", event.getTargetZ());
         event.setAttackDamage(0);
       }
     }
@@ -56,9 +61,18 @@ public class ModMountedPearl {
         return;
       }
       int setride = player.getPersistentData().getInt(NBT_RIDING_ENTITY);
-      if (setride > 0 && player.getRidingEntity() == null) {
+      int timer = player.getPersistentData().getInt(NBT_RIDING_TIMER);
+      if (setride > 0) {// && player.getRidingEntity() == null
         Entity horse = player.world.getEntityByID(setride);
         if (horse != null) {
+          if (timer > 0) {
+            player.getPersistentData().putInt(NBT_RIDING_TIMER, timer - 1);
+            double x = player.getPersistentData().getDouble("mpx");
+            double y = player.getPersistentData().getDouble("mpy");
+            double z = player.getPersistentData().getDouble("mpz");
+            horse.setPositionAndUpdate(x, y, z);
+            return;
+          }
           player.startRiding(horse, true);
           player.getPersistentData().putInt(NBT_RIDING_ENTITY, -1);
         }
